5f171ba12ca7beb5fd4f11e65067e49c84b3a72f
Subhi YD <subhy809fr@gmail.com>
2023-07-26 16:12:36 +0200

    1.1.0 create static ProductList, show product list with two way binding

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class ProductsComponent {
 
+  products: Array<any> = [
+    { id : 1, productName:"Asus PC", pixel:"1980 Px", price: 800, checked: false },
+    { id : 2, productName:"Dell Monitor", pixel:"2000 Px", price: 256, checked: true },
+    { id : 3, productName:"Samsung A21", pixel:"800 Px", price: 240, checked: true },
+    { id : 4, productName:"Iphone X Max", pixel:"1000 Px", price: 990, checked: true },
+    { id : 5, productName:"Nokia Phone", pixel:"600 Px", price: 350, checked: false }
+  ];
+
+  changeProductChecked(product: any) {
+    product.checked = !product.checked;
+  }
 }
